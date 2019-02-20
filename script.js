d6a69e203ec770d03192a0e78385c8bfdd1290f3
IvanPE <buluivan3@gmail.com>
2019-02-21 00:29:53 +0200

    Create font end design

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const cover = document.querySelector('#entry');
+cover.style.paddingTop = "35px";
+
+
+const buttons = document.querySelector('#buttons');
+buttons.style.paddingLeft = "5px";
+buttons.style.paddingRight = "5px";
+
+const display = document.querySelector('#display');
+display.style.borderRadius = "8px";
+display.style.width = "80%";
+display.style.height = "35px";
